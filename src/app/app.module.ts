6ff57e977046b32b573d5c5b6009d1c565181dfe
Phuc Chau <phucc@intelisysaviation.com>
2021-07-14 15:54:55 +0700

    Add *ng function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExNgStyleComponent } from './ex-ng-style/ex-ng-style.component';
+import { ExNgClassComponent } from './ex-ng-class/ex-ng-class.component';
+import { ExNgForComponent } from './ex-ng-for/ex-ng-for.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExNgStyleComponent,
+    ExNgClassComponent,
+    ExNgForComponent
   ],
   imports: [
     BrowserModule
